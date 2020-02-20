0d3abd7a59784d008bce182dcb72262768eec61e
erovelo <eric.1160@hotmail.com>
2020-02-20 17:51:38 -0600

    creacion vistas login y signup con navegacion entre ellas

---
+++ src/views/SignUp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="sign-up">
+      <p>Let's create a new account !</p>
+      <input type="text" placeholder="Email"><br>
+      <input type="password" placeholder="Password"><br>
+      <button>Sign Up</button>
+      <span>or go back to <router-link to="/login">login</router-link></span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'signUp',
+    data() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
